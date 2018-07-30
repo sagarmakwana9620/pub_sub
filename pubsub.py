54db1ca0646758fe08168b558b9b90fabc63c73c
SAGAR MAKWANA <sagar.makwana@quantiphi.com>
2018-07-30 18:46:15 +0530

    second commit

---
+++ pubsub.py
@@ -0,0 +1,141 @@
+import base64
+import json
+import argparse
+import os
+import time
+
+import googleapiclient.discovery
+from six.moves import input
+
+# [START delete_instance]
+def delete_instance(compute, project, zone, name):
+    return compute.instances().delete(
+        project=project,
+        zone=zone,
+        instance=name).execute()
+# [END delete_instance]
+
+def create_instance(compute, project, zone, name, bucketTo, bucketFrom):
+    # Get the latest Debian Jessie image.
+    image_response = compute.images().getFromFamily(
+        project='debian-cloud', family='debian-8').execute()
+    source_disk_image = image_response['selfLink']
+
+    # Configure the machine
+    machine_type = "zones/%s/machineTypes/n1-standard-1" % zone
+    startup_script = "#!/bin/bash\ngsutil cp %s %s" %(bucketFrom,bucketTo)
+    print(startup_script)
+    image_url = "http://storage.googleapis.com/gce-demo-input/photo.jpg"
+    image_caption = "Ready for dessert?"
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            'items': [{
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                'key': 'startup-script',
+                'value': startup_script 
+            }, {
+                'key': 'bucketTo',
+                'value': bucketTo
+            }, {
+                'key': 'bucketFrom',
+                'value': bucketFrom
+            }]
+        }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
+# [END create_instance]
+
+# [START wait_for_operation]
+def wait_for_operation(compute, project, zone, operation):
+    print('Waiting for operation to finish...')
+    while True:
+        result = compute.zoneOperations().get(
+            project=project,
+            zone=zone,
+            operation=operation).execute()
+
+        if result['status'] == 'DONE':
+            print("done.")
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(1)
+# [END wait_for_operation]
+
+# [START run]
+def main(project, bucketTo, bucketFrom, zone, instance_name, wait=True):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+
+    print('Creating instance.')
+
+    operation = create_instance(compute, project, zone, instance_name, bucketTo, bucketFrom)
+    wait_for_operation(compute, project, zone, operation['name'])
+    
+    print('Instance Created')
+    
+    print('Deleting Instance')
+    
+    operation = delete_instance(compute, project, zone, instance_name)
+    wait_for_operation(compute, project, zone, operation['name'])
+    
+    print('Instance Deleted')
+    
+    
+# [END run]
+
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    jsonmsg = json.loads(pubsub_message)
+    print(jsonmsg['project'])
+    print(jsonmsg['bucketTo'])
+    print(jsonmsg['bucketFrom'])
+    print(jsonmsg['zone'])
+    print(jsonmsg['instance_name'])
+    main(jsonmsg['project'],jsonmsg['bucketTo'],jsonmsg['bucketFrom'],jsonmsg['zone'],jsonmsg['instance_name'])
+    
